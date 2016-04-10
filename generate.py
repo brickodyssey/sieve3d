0e7cdfc0d332f259ba82979e499904c20de6d619
Blue Wizard <bluewizard@brickodyssey.com>
2016-04-10 15:21:13 -0700

    Regular hexagonal mesh.
    Add adjustable param to fill up as much of the sieve as possible.
    All float operations

--- generate.py
+++ generate.py
@@ -3,7 +3,14 @@ from solid import *
 from solid.utils import *
 
 # General parameter of the sieves (mm)
-hole_diam = 10
+
+# holes diameter
+hole_diam = 18
+# distance between holes
+sep = 1.25
+
+# other parameters that will probably not change
+output_file = 'sieve_%d.scad' % hole_diam
 box_size = 190
 box_thickness = 5
 box_height = 40
@@ -11,55 +18,56 @@ box_height = 40
 # hole radius
 hr = hole_diam / 2.
 
-# hole spacing
-hsp = hole_diam * 1.4
+# hole spacing x
+hspx = hole_diam * sep
+# hole spacing y
+hspy = hole_diam * sep * sqrt(3.)/2.
 
 base = difference()(
-    translate([-box_size/2 + box_thickness, -box_size/2 + box_thickness, -box_height/2.])(
+    translate([-box_size/2. + box_thickness, -box_size/2. + box_thickness, -box_height/2.])(
         minkowski()(
-            cube([box_size-2*box_thickness, box_size-2*box_thickness, box_height/2.]),
+            cube([box_size-2.*box_thickness, box_size-2.*box_thickness, box_height/2.]),
             cylinder(r=5, h=box_height/2.))),
     translate([0, 0, box_thickness])(
-        cube([box_size - 2 * box_thickness,
-              box_size - 2 * box_thickness,
+        cube([box_size - 2. * box_thickness,
+              box_size - 2. * box_thickness,
               box_height], center=True)
     )
 )
 
 # sieve pile up
 cw = box_size/2. - box_thickness*3/4.
-bottom_guides = (translate([cw, cw,-box_height/2])(sphere(2)) +
-                 translate([cw, -cw,-box_height/2])(sphere(2)) +
-                 translate([-cw, cw,-box_height/2])(sphere(2)) +
-                 translate([-cw, -cw,-box_height/2])(sphere(2)))
-top_guides = (translate([cw, cw, box_height/2])(sphere(1.5)) +
-              translate([cw, -cw, box_height/2])(sphere(1.5)) +
-              translate([-cw, cw, box_height/2])(sphere(1.5)) +
-              translate([-cw, -cw, box_height/2])(sphere(1.5)))
+bottom_guides = (translate([cw, cw,-box_height/2.])(sphere(2.)) +
+                 translate([cw, -cw,-box_height/2.])(sphere(2.)) +
+                 translate([-cw, cw,-box_height/2.])(sphere(2.)) +
+                 translate([-cw, -cw,-box_height/2.])(sphere(2.)))
+top_guides = (translate([cw, cw, box_height/2.])(sphere(1.5)) +
+              translate([cw, -cw, box_height/2.])(sphere(1.5)) +
+              translate([-cw, cw, box_height/2.])(sphere(1.5)) +
+              translate([-cw, -cw, box_height/2.])(sphere(1.5)))
 
 # odd lines holes
-x_range = int(math.floor((box_size / 2. - box_thickness - hr) / hsp))
-y_range = int(math.floor((box_size / 2. - box_thickness - hr) / hsp / 2))
+x_range = int(math.floor((box_size / 2. - box_thickness - hr) / hspx))
+y_range = int(math.floor((box_size / 2. - box_thickness - hr) / hspy / 2.))
 
 l =[]
 for i in range(-x_range, x_range + 1):
     for j in range(-y_range, y_range + 1):
-        l.append(translate([i*hsp, j*hsp*2, 0])(
+        l.append(translate([i*hspx, j*hspy*2., 0])(
             cylinder(r=hr, h=100, center=True))
-
-             )
+        )
 
 # even lines holes
-x_range = int(math.floor((box_size / 2. - box_thickness) / hsp))
-y_range = int(math.floor((box_size / 2. - box_thickness) / hsp / 2))
+x_range = int(math.floor((box_size / 2. - box_thickness) / hspx))
+y_range = int(math.floor((box_size / 2. - box_thickness) / hspy / 2.))
 
 l2 = []
 for i in range(-x_range, x_range):
     for j in range(-y_range, y_range):
-        l2.append(translate([(i + 1/2.)*hsp, (2*j + 1)*hsp, 0])(
+        l2.append(translate([(i + 1/2.)*hspx, (2.*j + 1)*hspy, 0])(
                 cylinder(r=hr, h=100, center=True))
         )
 
 d = base - l - l2 - bottom_guides + top_guides
 
-scad_render_to_file(d, 'sieve1.scad')
+scad_render_to_file(d, output_file)
